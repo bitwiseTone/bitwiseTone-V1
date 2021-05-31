1e28d3709ba185edc5594741cfdad684ec2f6c6e
laino <laino@Rebog.local>
2021-05-31 20:52:14 +0700

    update procfile

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', './src/views');
 
 app.set("view engine", "ejs");
 
